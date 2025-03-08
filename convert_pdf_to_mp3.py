6d06447925c4f22fe8afe1fdec23d676401ba379
ambiernat <43310781+ambiernat@users.noreply.github.com>
2025-03-08 19:47:09 +0000

    Update convert_pdf_to_mp3.py

--- convert_pdf_to_mp3.py
+++ convert_pdf_to_mp3.py
@@ -51,32 +51,34 @@ def main():
     """
     Main function to handle user input for single file or directory processing.
     """
-    mode = input("Press Enter to upload a single file or type 'dir' to process an entire directory: ").strip()
+    #mode = input("Press Enter to upload a single file or type 'dir' to process an entire directory: ").strip()
 
-    if mode.lower() == 'dir':
-        directory = input("Enter the path to the directory on Google Drive (this can be obtained by browsing your Google Drive on the left hand column and clicking the three dots and selecting 'Copy Path'): ").strip()
-        output_dir = directory
+    #if mode.lower() == 'dir':
+    directory = input("Enter the path to the directory on Google Drive (this can be obtained by browsing your Google Drive on the left hand column and clicking the three dots and selecting 'Copy Path'): ").strip()
+    output_dir = directory
 
-        mp3_paths = []
-        for filename in os.listdir(directory):
-            if filename.endswith('.pdf'):
-                file_path = os.path.join(directory, filename)
-                print(f"Processing {file_path}...")
-                mp3_path = process_pdf_file(file_path, output_dir)
-                mp3_paths.append(mp3_path)
+    mp3_paths = []
+    for filename in os.listdir(directory):
+        if filename.endswith('.pdf'):
+            file_path = os.path.join(directory, filename)
+            print(f"Processing {file_path}...")
+            mp3_path = process_pdf_file(file_path, output_dir)
+            mp3_paths.append(mp3_path)
 
-        if mp3_paths:
-            zip_path = zip_files(output_dir)
-            files.download(zip_path)
-    else:
-        try:
-            filename = upload_pdf_file()
-            mp3_path = process_pdf_file(filename, '/content')
-            files.download(mp3_path)
-        except Exception as e:
-            print(f"An error occurred: {e}")
+    if mp3_paths:
+        zip_path = zip_files(output_dir)
+        files.download(zip_path)
+    # else:
+    #     try:
+    #         filename = upload_pdf_file()
+    #         mp3_path = process_pdf_file(filename, '/content')
+    #         files.download(mp3_path)
+    #     except Exception as e:
+    #         print(f"An error occurred: {e}")
 
 # if 'google.colab' in str(get_ipython()):
- #   main()
-#else:
- #   print('This code is intended to run in Google Colab.')
+#    main()
+# else:
+#    print('This code is intended to run in Google Colab.')
+
+main()
